3ccd9ab5a3d597ab2d66d61f1196f1dbf0a504bd
YonathanFabiano <yonathan.fabiano@si.ukdw.ac.id>
2023-05-15 13:18:46 +0700

    Pertemuan 10: Swashbuckle

--- BookStoreApi/Controllers/BooksController.cs
+++ BookStoreApi/Controllers/BooksController.cs
@@ -1,7 +1,6 @@
 using BookStoreApi.Models;
 using BookStoreApi.Services;
 using Microsoft.AspNetCore.Mvc;
-using System.Net;
 
 namespace BookStoreApi.Controllers;
 
@@ -14,45 +13,142 @@ public class BooksController : ControllerBase
     public BooksController(BooksService booksService) =>
         _booksService = booksService;
 
+    /// <summary>
+    /// get
+    /// </summary>
+    /// <param name=""></param>
+    /// <returns>A newly created TodoItem</returns>
+    /// <remarks>
+    /// Sample request:
+    ///
+    ///     POST /Todo
+    ///     {
+    ///        "id": 1,
+    ///        "name": "Item #1",
+    ///        "isComplete": true
+    ///     }
+    ///
+    /// </remarks>
+    /// <response code="201">Returns the newly created item</response>
+    /// <response code="400">If the item is null</response>
+    /// <response code="401">HTTP request has not been applied because it lacks valid authentication credentials for the requested resource</response>
+    /// <response code="404">The server was unable to find the requested resource</response>
+    /// <response code="500">The server encountered an unexpected condition that prevented it from fulfilling the request</response>
     [HttpGet]
+    [ProducesResponseType(StatusCodes.Status201Created)]
+    [ProducesResponseType(StatusCodes.Status400BadRequest)]
+    [ProducesResponseType(StatusCodes.Status401Unauthorized)]
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
+    [ProducesResponseType(StatusCodes.Status500InternalServerError)]
     public async Task<List<Book>> Get() =>
         await _booksService.GetAsync();
 
+    /// <summary>
+    /// get id
+    /// </summary>
+    /// <param name="id"></param>
+    /// <returns>A newly created TodoItem</returns>
+    /// <remarks>
+    /// Sample request:
+    ///
+    ///     POST /Todo
+    ///     {
+    ///        "id": 1,
+    ///        "name": "Item #1",
+    ///        "isComplete": true
+    ///     }
+    ///
+    /// </remarks>
+    /// <response code="201">Returns the newly created item</response>
+    /// <response code="400">If the item is null</response>
+    /// <response code="401">HTTP request has not been applied because it lacks valid authentication credentials for the requested resource</response>
+    /// <response code="404">The server was unable to find the requested resource</response>
+    /// <response code="500">The server encountered an unexpected condition that prevented it from fulfilling the request</response>
     [HttpGet("{id:length(24)}")]
-    public async Task<ActionResult<Book>> Get(string id){
+    [ProducesResponseType(StatusCodes.Status201Created)]
+    [ProducesResponseType(StatusCodes.Status400BadRequest)]
+    [ProducesResponseType(StatusCodes.Status401Unauthorized)]
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
+    [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+    public async Task<ActionResult<Book>> Get(string id)
+    {
         var book = await _booksService.GetAsync(id);
 
-        if (book is null){
+        if (book is null)
+        {
             return NotFound();
         }
 
         return book;
     }
 
-    // [HttpPost]
-    // public async Task<IActionResult> Post(Book newBook){
-    //     await _booksService.CreateAsync(newBook);
+    /// <summary>
+    /// post
+    /// </summary>
+    /// <param name="newBook"></param>
+    /// <returns>A newly created TodoItem</returns>
+    /// <remarks>
+    /// Sample request:
+    ///
+    ///     POST /Todo
+    ///     {
+    ///        "id": 1,
+    ///        "name": "Item #1",
+    ///        "isComplete": true
+    ///     }
+    ///
+    /// </remarks>
+    /// <response code="201">Returns the newly created item</response>
+    /// <response code="400">If the item is null</response>
+    /// <response code="401">HTTP request has not been applied because it lacks valid authentication credentials for the requested resource</response>
+    /// <response code="404">The server was unable to find the requested resource</response>
+    /// <response code="500">The server encountered an unexpected condition that prevented it from fulfilling the request</response>
+    [HttpPost]
+    [ProducesResponseType(StatusCodes.Status201Created)]
+    [ProducesResponseType(StatusCodes.Status400BadRequest)]
+    [ProducesResponseType(StatusCodes.Status401Unauthorized)]
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
+    [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+    public async Task<IActionResult> Post(Book newBook)
+    {
+        await _booksService.CreateAsync(newBook);
 
-    //     return CreatedAtAction(nameof(Get), new { id = newBook.Id }, newBook);
-    // }
-    
-    public HttpResponseMessage Post(Book newBook){
-        if (ModelState.IsValid){
-            // Do something with the product (not shown).
-
-            return new HttpResponseMessage(HttpStatusCode.OK);
-        }
-        else{
-            return new HttpResponseMessage(HttpStatusCode.BadRequest);
-        }
+        return CreatedAtAction(nameof(Get), new { id = newBook.Id }, newBook);
     }
 
-    
+    /// <summary>
+    /// put
+    /// </summary>
+    /// <param name="updatedBook"></param>
+    /// <returns>A newly created TodoItem</returns>
+    /// <remarks>
+    /// Sample request:
+    ///
+    ///     POST /Todo
+    ///     {
+    ///        "id": 1,
+    ///        "name": "Item #1",
+    ///        "isComplete": true
+    ///     }
+    ///
+    /// </remarks>
+    /// <response code="201">Returns the newly created item</response>
+    /// <response code="400">If the item is null</response>
+    /// <response code="401">HTTP request has not been applied because it lacks valid authentication credentials for the requested resource</response>
+    /// <response code="404">The server was unable to find the requested resource</response>
+    /// <response code="500">The server encountered an unexpected condition that prevented it from fulfilling the request</response>
     [HttpPut("{id:length(24)}")]
-    public async Task<IActionResult> Update(string id, Book updatedBook){
+    [ProducesResponseType(StatusCodes.Status201Created)]
+    [ProducesResponseType(StatusCodes.Status400BadRequest)]
+    [ProducesResponseType(StatusCodes.Status401Unauthorized)]
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
+    [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+    public async Task<IActionResult> Update(string id, Book updatedBook)
+    {
         var book = await _booksService.GetAsync(id);
 
-        if (book is null){
+        if (book is null)
+        {
             return NotFound();
         }
 
@@ -63,11 +159,39 @@ public class BooksController : ControllerBase
         return NoContent();
     }
 
+    /// <summary>
+    /// delete
+    /// </summary>
+    /// <param name="id"></param>
+    /// <returns>A newly created TodoItem</returns>
+    /// <remarks>
+    /// Sample request:
+    ///
+    ///     POST /Todo
+    ///     {
+    ///        "id": 1,
+    ///        "name": "Item #1",
+    ///        "isComplete": true
+    ///     }
+    ///
+    /// </remarks>
+    /// <response code="201">Returns the newly created item</response>
+    /// <response code="400">If the item is null</response>
+    /// <response code="401">HTTP request has not been applied because it lacks valid authentication credentials for the requested resource</response>
+    /// <response code="404">The server was unable to find the requested resource</response>
+    /// <response code="500">The server encountered an unexpected condition that prevented it from fulfilling the request</response>
     [HttpDelete("{id:length(24)}")]
-    public async Task<IActionResult> Delete(string id){
+    [ProducesResponseType(StatusCodes.Status201Created)]
+    [ProducesResponseType(StatusCodes.Status400BadRequest)]
+    [ProducesResponseType(StatusCodes.Status401Unauthorized)]
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
+    [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+    public async Task<IActionResult> Delete(string id)
+    {
         var book = await _booksService.GetAsync(id);
 
-        if (book is null){
+        if (book is null)
+        {
             return NotFound();
         }
 
