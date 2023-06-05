c0d185d262db0548499ca8098e7dc2e694f3247c
YonathanFabiano <yonathan.fabiano@si.ukdw.ac.id>
2023-06-05 12:43:56 +0700

    Pertemuan 12 Web Client API

---
+++ BookStoreBlazor/BookStore.cs
@@ -0,0 +1,8 @@
+public class BookStore
+{
+    public string? Id { get; set; }
+    public string? BookName { get; set; }
+    public decimal? Price { get; set; }
+    public string? Category { get; set; }
+    public string? Author { get; set; }
+}
